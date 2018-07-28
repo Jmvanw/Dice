4554bfb35c7cba3cfb8fd34ea5b46fc3e4487811
jmvan <jmvanw@gmail.com>
2018-07-28 16:49:00 -0700

    Comment out unused dice methods

--- DiceRoller/DiceRoller/Dice.cs
+++ DiceRoller/DiceRoller/Dice.cs
@@ -8,47 +8,47 @@ namespace DiceRoller
 {
     class Dice
     {
-        public static int D4()
-        {
-            Random roll = new Random();
-            int result = roll.Next(1, 5);
-            return result;
-        }
+        //public static int D4()
+        //{
+        //    Random roll = new Random();
+        //    int result = roll.Next(1, 5);
+        //    return result;
+        //}
 
-        public static int D6()
-        {
-            Random roll = new Random();
-            int result = roll.Next(1, 7);
-            return result;
-        }
+        //public static int D6()
+        //{
+        //    Random roll = new Random();
+        //    int result = roll.Next(1, 7);
+        //    return result;
+        //}
 
-        public static int D8()
-        {
-            Random roll = new Random();
-            int result = roll.Next(1, 9);
-            return result;
-        }
+        //public static int D8()
+        //{
+        //    Random roll = new Random();
+        //    int result = roll.Next(1, 9);
+        //    return result;
+        //}
 
-        public static int D10()
-        {
-            Random roll = new Random();
-            int result = roll.Next(1, 11);
-            return result;
-        }
+        //public static int D10()
+        //{
+        //    Random roll = new Random();
+        //    int result = roll.Next(1, 11);
+        //    return result;
+        //}
 
-        public static int D12()
-        {
-            Random roll = new Random();
-            int result = roll.Next(1, 13);
-            return result;
-        }
+        //public static int D12()
+        //{
+        //    Random roll = new Random();
+        //    int result = roll.Next(1, 13);
+        //    return result;
+        //}
 
-        public static int D20()
-        {
-            Random roll = new Random();
-            int result = roll.Next(1, 21);
-            return result;
-        }
+        //public static int D20()
+        //{
+        //    Random roll = new Random();
+        //    int result = roll.Next(1, 21);
+        //    return result;
+        //}
 
         public static int[] AnyDice(int size, int times,int mod)
         {
